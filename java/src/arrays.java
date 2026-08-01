20f4816cf5405c38a02bb34773093e2c809d510a
Sujal Mishra <sujalmishra68@gmail.com>
2026-08-01 22:18:22 +0530

    1august2026

--- java/src/arrays.java
+++ java/src/arrays.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class arrays {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);  //
 
         System.out.println("Enter the number of elements");
         int n = sc.nextInt();
