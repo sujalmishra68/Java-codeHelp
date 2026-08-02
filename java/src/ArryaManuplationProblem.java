52597962601a1bd92669dd2a1a176cf1781c3105
Sujal Mishra <sujalmishra68@gmail.com>
2026-08-02 23:56:10 +0530

    arraymanuplation

--- java/src/ArryaManuplationProblem.java
+++ java/src/ArryaManuplationProblem.java
@@ -1,18 +1,29 @@
+import java.sql.SQLOutput;
+import java.util.HashMap;
+
 public class ArryaManuplationProblem {
   public static void main(String[] args) {
      
     //array for reversing the array
     int [] arr ={1,2,32,4,69,10};
-   System.out.println("-----------Reverse the array----------");
-reversearray(arr);
+    int[] arr2 ={1,2,3,4,5,2};
+//   System.out.println("-----------Reverse the array----------");
+//reversearray(arr);
+//
+//System.out.println();
+//System.out.println("-----------Shift the array----------");
+//shiftarray(arr);
 
-System.out.println();
-System.out.println("-----------Shift the array----------");
-shiftarray(arr);
+//    System.out.println("----------alternate mannner");
+//    ALternateArray(arr2);
+
+    ModeArray(arr2);
    
 
   }
 
+
+  //--------------------Function Begain--------------------------->
   //function to reverse the array
   static void reversearray(int [] arr){
     System.out.print("The original array is: ");
@@ -59,4 +70,33 @@ shiftarray(arr);
   }
 
   //print extreme elements  in alternate manner
+  static void ALternateArray(int [] arr2){
+    int i=0;
+    int n= arr2.length;
+    int j= arr2.length-1;
+    while(i<=j){
+      if(i==j){
+        System.out.println(arr2[i]);
+      }else{
+        System.out.println(arr2[i]);
+        System.out.println(arr2[j]);
+      }
+        i++;
+        j--;
+    }
+
+
+  }
+
+
+  // Find the mode of an array
+  static void ModeArray(int[] arr){
+    HashMap<Integer,Integer> map= new HashMap<>();
+
+    for (int num : arr) {
+      map.put(num, map.getOrDefault(num, 0) + 1);
+    }
+
+
+  }
 }
